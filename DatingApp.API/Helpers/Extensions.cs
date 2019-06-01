d585f8d9e408bfa3ee626e78e944856b088c3850
Pravin Desai <pravindesai0790@gmail.com>
2019-06-02 00:24:25 +0530

    Add 2 New Class & Extension method for Pagination

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,22 +1,31 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace DatingApp.API.Helpers
 {
     public static class Extensions
     {
         // Extension method to add own header to http responce
-        public static void AddApplicationError(this HttpResponse response, string message) 
+        public static void AddApplicationError(this HttpResponse response, string message)
         {
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Header", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
 
+        public static void AddPagination(this HttpResponse response,
+            int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Header", "Pagination");
+        }
+
         public static int CalculateAge(this DateTime theDateTime)
         {
             var age = DateTime.Today.Year - theDateTime.Year;
-            if(theDateTime.AddYears(age) > DateTime.Today)
+            if (theDateTime.AddYears(age) > DateTime.Today)
                 age--;
 
             return age;
